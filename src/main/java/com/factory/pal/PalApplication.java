6a61b4e22ca5ef21314b5b7ba7ae28594ce79c06
Oitejjho Dutta <oitejjhodutta@AGC-FVFD703AP3YV.local>
2022-01-29 14:35:22 +0700

    code added

---
+++ src/main/java/com/factory/pal/PalApplication.java
@@ -0,0 +1,13 @@
+package com.factory.pal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/pal/PalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.pal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
